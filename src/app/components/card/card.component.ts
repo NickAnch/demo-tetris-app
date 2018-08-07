757d1718ca5586c8df4384cba4c8b5789b5c97a8
NickAnch <kolya_anch@mail.ru>
2018-08-07 11:16:47 +0300

    add animation

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -6,7 +6,7 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent {
-  @Input() persons;
+  @Input() person;
 
   constructor() { }
 
